4e99838c6002522394d2bf8101153ed4f3e2dcfa
Disease <83523587+CPScript@users.noreply.github.com>
2024-02-11 16:38:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import os # required
 from os import system # required
 from subprocess import call
 
-print("Loading") # alert
+def clear():
+ os.system('cls' if os.name == 'nt' else 'clear')
 
-time.sleep(1) # sleep
-os.system('clear') # Might not work as this mainly works for 'unix' shells
 
-print("Done, launching ray tracer") # alert that the 'clear' command worked
-os.sysytem('clear')
+print("Loading!!!") # alert
+print("Done, launching ray-tracer") # alert that the 'clear' command worked
+time.sleep(1)
+clear()
 
-print("""Depending on your calculator model this might crash your system and or worst might not work!
-Raytracing software!""") # DISCLAIMER
+print("""
+Depending on your calculator model this might crash your system!
+And or might not work on your calculator!
+""") # DISCLAIMER
 
 time.sleep(5)
-os.system('clear')
-
+clear()
 
 print("""
 
@@ -39,6 +41,7 @@ choice = input("Make Number Selection :")
 
 if choice == "1":
     print("Loading...")
+    clear()
     call(["python", "r/main.py"])
 
 if choice == "2":
